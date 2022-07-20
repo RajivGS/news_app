42d22b390f6fa2590cc710f82396ad24b872f0a1
Rajiv Gopal Singh <rajivgopalsingh@gmail.com>
2022-07-20 22:50:03 +0545

    splash_page complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/views/home.dart';
+import 'package:news_app/pages/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: SplashScreen(),
     );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/components/custom_list_tile.dart';
-import 'package:news_app/helper/api_services.dart';
 import 'package:news_app/models/article_model.dart';
+import 'package:news_app/services/api_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,27 +14,25 @@ class _HomePageState extends State<HomePage> {
   ApiService client = ApiService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("News App"),
-      // ),
-      body: FutureBuilder(
-        future: client.getArticle(),
-        builder: ((context, AsyncSnapshot<List<Article>> snapshot) {
-          if (snapshot.hasData) {
-            List<Article> articles = snapshot.requireData;
-            return ListView.builder(
-              itemCount: articles.length,
-              itemBuilder: (context, index) {
-                return customListTile(articles[index]);
-              },
+    return SafeArea(
+      child: Scaffold(
+        body: FutureBuilder(
+          future: client.getArticle(),
+          builder: ((context, AsyncSnapshot<List<Article>> snapshot) {
+            if (snapshot.hasData) {
+              List<Article> articles = snapshot.requireData;
+              return ListView.builder(
+                itemCount: articles.length,
+                itemBuilder: (context, index) {
+                  return customListTile(articles[index]);
+                },
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          }
-
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }),
+          }),
+        ),
       ),
     );
   }
